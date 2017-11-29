c13a2ecf0000b721bd267561bb3d0f194a9aff70
Arturo Salmeron <asalmeron3@gatech.edu>
2017-11-28 20:13:03 -0500

    Setting up Routes/Server

---
+++ config/connection.js
@@ -0,0 +1,40 @@
+//______________  Require mySQL node package   _______________
+
+	var mysql = require("mysql");
+//____________________________________________________________
+
+
+
+
+//__________________  Connect Node to mySQL  _________________
+
+	var connection = mysql.createConnection({
+		host:"localhost",
+		user:"root",
+		password: "",
+		database: ""
+	});	
+//____________________________________________________________
+
+
+
+//___________________  Connect to your host  _________________
+//
+// Find and insert different types of connection errors 
+
+	connection.connect(function(err){
+		if (err){
+			console.log("There was an error connecting: " + err.stack);
+			return;
+		}
+		console.log("ChangeYourJob connection is up as id "+ connection.threadId);
+	});
+//___________________________________________________________
+
+
+
+//___________________  Export the Connection  _________________
+	
+	module.exports = connection;
+
+//___________________________________________________________
