9d2c3df18a39a4bf2b5044424f6411032c270cf5
Lina C <linaclark96@gmail.com>
2017-12-02 13:32:56 -0500

    Removed comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,3 @@
-//here is where I will add in the work for the sequelize routes
-
-/*sequelize.query('SELECT * FROM projects WHERE status = ?',
-  { replacements: ['active'], type: sequelize.QueryTypes.SELECT }
-).then(projects => {
-  console.log(projects)
-})*/
-
-//the above is how I will map the query from the api routes to the html
-
 var db = require("../models");
 
 
