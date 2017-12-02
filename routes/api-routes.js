961f9eb782fab3fa7d765c64112514f1da42fee6
Lina C <linaclark96@gmail.com>
2017-12-02 12:30:06 -0500

    Updated api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,42 +10,43 @@
 
 var db = require("../models");
 
-module.exports = function(app) {
-    app.post("/api/", function(req, res) {
-        console.log(req.body);
-        db.User.create({
-            Age: ,
-            BusinessTravel: ,
-            DailyRate: ,
-            DistanceFromHome: ,
-            Education: ,
-            EnvironmentSatisfaction: ,
-            Gender: ,
-            HourlyRate: ,
-            JobInvolvement: ,
-            JobSatisfaction: ,
-            MaritalStatus: ,
-            MonthlyIncome: ,
-            NumCompaniesWorked: ,
-            OverTime: ,
-            PercentSalaryHike: ,
-            PerformanceRating: ,
-            RelationshipSatisfaction: ,
-            StandardHours: StockOptions: ,
-            TrainingTimesLastYear: ,
-            WorkLifeBalance: ,
-            YearsAtCompany: ,
-            YearsInCurrentRole: ,
-            YearsSinceLastPromotion: ,
-            YearsWithCurrManager: ,
-            AbsentHours: ,
-        });
-        .then(function(dbPost) {
-            res.json(dbPost);
-        });
-    });
+var derivedTable =
 
-    app.get("/api/", function(req, res) {
-        db.User
-    })
-}
+    module.exports = function(app) {
+        app.post("/api/", function(req, res) {
+            console.log(req.body);
+            db.User.create({
+                Age: req.body[0],
+                Gender: req.body[1],
+                MaritalStatus: req.body[2],
+                Education: req.body[3],
+                StandardHours: req.body[4],
+                BusinessTravel: req.body[5],
+                DistanceFromHome: req.body[6],
+                OverTime: req.body[7],
+                DailyRate: req.body[8],
+                AbsentHours: req.body[9],
+                WorkLifeBalance: req.body[10],
+                StockOptions: req.body[11],
+                PercentSalaryHike: req.body[12],
+                TrainingTimesLastYear: req.body[13],
+                YearsSinceLastPromotion: req.body[14],
+                PerformanceRating: req.body[15],
+                HourlyRate: req.body[16],
+                JobInvolvement: req.body[17],
+                EnvironmentSatisfaction: req.body[18],
+                RelationshipSatisfaction: req.body[19],
+                YearsAtCompany: req.body[20],
+                YearsWithCurrManager: req.body[21],
+                NumCompaniesWorked: req.body[22],
+            });
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+        });
+
+        app.get("/api/", function(req, res) {
+                    db.User.query('Select AVG(?) from (SELECT * FROM glassdoor_comments WHERE MaritalStatus= ? AND Education = ? AND StandardHours = 80 AND BusinessTravel = ?)', { replacements: ['active'], type: sequelize.QueryTypes.SELECT }).then(projects => {
+                        console.log(projects);
+                    })
+                }
